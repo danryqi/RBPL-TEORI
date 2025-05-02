c4be8b5924f62bb5c61ed80f0e15b1545eb67f38
IsDukeThere <masduqialfiyan.acc@gmail.com>
2025-05-02 16:16:48 +0700

    update, dashboard layout

--- app/views/admin/admin-index.php
+++ app/views/admin/admin-index.php
@@ -1,6 +1,44 @@
+<main>
+  <table class="main mx-auto">
+    <tr class="info">
+      <td>
+        <div class="produk-container"></div>
+      </td>
+      <td>
+        <div class="stok-rendah"></div>
+      </td>
+      <td>
+        <div class="penjualan-harian"></div>
+      </td>
+      <td>
+        <div class="pesanan-baru"></div>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2">
+        <div class="tren-penjualan"></div>
+      </td>
+      <td colspan="2">
+        <div class="kategori-terjual"></div>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2">
+        <div class="stok-produk"></div>
+      </td>
+      <td colspan="2">
+        <div class="produk-terlaris"></div>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="4">
+        <div class="pembelian-terbaru"></div>
+      </td>
+    </tr>
+  </table>
+</main>
 
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
-
